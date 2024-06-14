c0c5774299982a56a86fbba966d22b9eac756a36
ChharngChhit <chhit085@gmail.com>
2024-06-14 13:38:44 +0700

    new update

--- database/migrations/2024_06_14_063317_create_students_table.php
+++ database/migrations/2024_06_14_063317_create_students_table.php
@@ -11,12 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('title');
-            $table->string('author');
-            $table->string('isbn');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->date('dob');
             $table->string('device_id')->nullable();
         });
     }
@@ -26,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('students');
     }
 };
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,18 @@
 <?php
-
+use App\Models\Student;
+use function PHPUnit\Framework\assertTrue;
 it('returns a successful response', function () {
     $response = $this->get('/');
 
     $response->assertStatus(200);
+});
+
+test('create student', function () {
+    $student = Student::create([
+        'name' => 'John Doe',
+        'email' => '<your_name>@itc.edu.kh',
+        'phone' => '012 345 678',
+        'dob' => '2003-10-01'
+    ]);
+    assertTrue($student->exists());
 });
