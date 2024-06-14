fe1955463ec1afd7486477df93c50a601b90e3e3
ChharngChhit <chhit085@gmail.com>
2024-06-14 16:57:00 +0700

    update migrate

--- database/migrations/2024_06_14_063317_create_students_table.php
+++ database/migrations/2024_06_14_063317_create_students_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('phone');
             $table->date('dob');
             $table->string('device_id')->nullable();
